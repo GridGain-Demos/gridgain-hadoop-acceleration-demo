7ae4d155319c0fa9f007da995c12c08d800d9f85
Rohit Dharampal <rohit.dharampal@gridgain.com>
2020-05-18 15:40:29 -0500

    setenv changes

--- scripts/setenv.bat
+++ scripts/setenv.bat
@@ -3,6 +3,6 @@ rem set JAVA_HOME and IGNITE_HOME either as Windows environment variables or in 
 rem set JAVA_HOME 
 rem set IGNITE_HOME
 rem set WEB_CONSOLE_HOME
-set WEB_CONSOLE_HOME=%TRAINING_HOME%\runtime\gridgain-web-console-2020.02.00
+set WEB_CONSOLE_HOME=%GGDEVTRAINEE_HOME%\runtime\gridgain-web-console-2020.02.00
 set USER_RT_FOLDER=%~dp0\..
 set GG_RT_FOLDER=%IGNITE_HOME%
