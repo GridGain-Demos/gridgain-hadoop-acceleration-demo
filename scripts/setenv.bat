7e6c7406f618128570c2727dbf045e0e899e1bdc
Rohit Dharampal <rohit.dharampal@gridgain.com>
2020-05-18 14:03:52 -0500

    Added scripts for node, web console and web agent

--- scripts/setenv.bat
+++ scripts/setenv.bat
@@ -1,4 +1,5 @@
 @echo off
+rem set JAVA_HOME and IGNITE_HOME either as Windows environment variables or in the script below. Set WEB_CONSOLE_HOME in the script below.
 rem set JAVA_HOME 
 rem set IGNITE_HOME
 rem set WEB_CONSOLE_HOME
