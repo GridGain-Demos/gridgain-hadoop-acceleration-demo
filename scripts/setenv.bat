621fcf2297afaaf8c960b128dbdfe2326fd9ffbc
Rohit Dharampal <rohit.dharampal@gridgain.com>
2020-05-18 14:01:59 -0500

    Added scripts for node, web console and web agent

---
+++ scripts/setenv.bat
@@ -0,0 +1,7 @@
+@echo off
+rem set JAVA_HOME 
+rem set IGNITE_HOME
+rem set WEB_CONSOLE_HOME
+set WEB_CONSOLE_HOME=%TRAINING_HOME%\runtime\gridgain-web-console-2020.02.00
+set USER_RT_FOLDER=%~dp0\..
+set GG_RT_FOLDER=%IGNITE_HOME%
---
+++ scripts/start-web-agent.bat
@@ -0,0 +1,14 @@
+@echo off
+
+rem Use local variables
+setlocal
+
+@call "setenv.bat"
+
+start "WebUI"  /D "%WEB_CONSOLE_HOME%\web-agent" /B gridgain-web-console-agent.bat -n http://127.0.0.1:7080
+
+endlocal
+title Command Prompt
+if "%OS%"=="Windows_NT" @endlocal
+if "%OS%"=="WINNT" @endlocal
+exit /B 0
